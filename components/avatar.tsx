41dd0ee5d0968d3dd8b1128641273f04a9f9345f
SKRTEEEEEE <adanreh.m@gmail.com>
2024-03-16 15:21:53 +0100

    test

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -7,8 +7,8 @@ import { MotionTransition } from "./transition-component";
 
 export function Avatar() {
     return (
-        <MotionTransition position="bottom" className="bottom-0 right-0 hidden md:inline-block md:absolute ">
-            <Image src="/avatar-1.png" width="400" height="400" className="w-full h-full " alt="Particles " />
+        <MotionTransition position="bottom" className="top-10 right-0 hidden md:inline-block md:absolute ">
+            <Image src="/avatar1.svg" width="600" height="600" className="" alt="Particles " />
         </MotionTransition>
     )
 }
